6477a6327589b891df0ed98e43cb1e770c5bde87
enguemeta <emmanuel.nguemeta-fomekong.1@ulaval.ca>
2019-11-22 21:59:14 -0500

    Updates

---
+++ precache-manifest.6dcbd6680e96e6cb75ebca3c71887a5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1269492bdfec744edbc1e8df82de6079",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "19019d6d21e1fa71c390",
+    "url": "/monsters-rolodex/static/css/main.5a94b322.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "19019d6d21e1fa71c390",
+    "url": "/monsters-rolodex/static/js/main.3c0e6eef.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
